da232a42eb3e7a0cfa2a16f09ed70939ea2f8c09
mackenziehodge <72561318+mackenziehodge@users.noreply.github.com>
2020-10-24 19:44:23 -0400

    made search engine functional using API data

--- src/app.js
+++ src/app.js
@@ -32,10 +32,19 @@ function displayTemperature(response){
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt",response.data.weather[0].description)
    
-}  
-   
+} 
+function search(city){
 let apiKey = "ab59ca7402d6fc239c3f8d540f4ab058";
-let city = "London"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+} 
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+} 
+
+search("New York");
+
+let form=document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
